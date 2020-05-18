715570663d97c55608ddfb3ac9815671019d7625
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-18 18:22:37 +0530

    Injucting data from Common layer

---
+++ ParkingBusinessLayer/Interface/IParkingBL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingCommonLayer.Services;
+using ParkingReposLayer.Services;
+
+namespace ParkingBusinessLayer.Interface
+{
+    public interface IParkingBL
+    {
+        bool ParkingDatails(ParkingCL data);
+    }
+}
---
+++ ParkingBusinessLayer/Services/ParkingBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingReposLayer.Interface;
+using ParkingReposLayer.Services;
+using ParkingCommonLayer.Services;
+using ParkingBusinessLayer.Interface;
+
+namespace ParkingBusinessLayer.Services
+{
+    public class ParkingBL : IParkingBL
+    {
+        private IParkingRL Parking;
+        public ParkingBL(IParkingRL data)
+        {
+            Parking = data;
+        }
+        public bool ParkingDatails(ParkingCL data)
+        {
+            try
+            {
+                var Result = Parking.ParkingDatails(data);
+                if (!Result.Equals(null))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
