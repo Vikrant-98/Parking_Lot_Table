b91e1af16099f0cbff81c7bc798dddf065af528f
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-18 18:23:31 +0530

    Declaring poco class and methods

---
+++ ParkingCommonLayer/Services/ParkingCL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingCommonLayer.Services
+{
+    public class ParkingCL
+    {
+        public int VehicalNo { get; set; }
+
+        public string VehicalBrand { get; set; }
+
+        public string VehicalColor { get; set; }
+
+        public int ChargePerHr { get; set; }
+
+        public DateTime EntryTime { get; set; }
+
+        public DateTime ExitTime { get; set; }
+
+        public int DriverCategory { get; set; }
+
+        public string ParkingType { get; set; }
+       
+    }
+}
