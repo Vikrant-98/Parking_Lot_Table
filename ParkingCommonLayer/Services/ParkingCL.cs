1a047deeb92f3a3bed519c3b7a56d9514f7183d4
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-20 18:36:54 +0530

    Login Details are added

--- ParkingCommonLayer/Services/ParkingCL.cs
+++ ParkingCommonLayer/Services/ParkingCL.cs
@@ -6,11 +6,13 @@ namespace ParkingCommonLayer.Services
 {
     public class ParkingCL
     {
-        public string ParkingId { get; set; }
+        public int ParkingId { get; set; }
 
         public int VehicalNo { get; set; }
 
         public string VehicalBrand { get; set; }
+
+        public string Password { get; set; }
 
         public string VehicalColor { get; set; }
 
@@ -24,5 +26,22 @@ namespace ParkingCommonLayer.Services
 
         public string ParkingType { get; set; }
        
+    }
+    public class EncryptedPassword
+    {
+        public static string EncodePasswordToBase64(string Password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[Password.Length];
+                encData_byte = Encoding.UTF8.GetBytes(Password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
     }
 }
